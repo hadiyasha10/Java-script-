0a429b398def9f24e6758c5eec760a16d7182f3a
hadiyasha10 <hadiyasheikh329@gmail.com>
2025-09-03 21:44:24 +0500

    done

---
+++ Assignment # 02/app.js
@@ -0,0 +1,55 @@
+// Q1. Declare a variable called username?
+
+// var username;
+
+// Q2. Declare a variable called myName & assign to it a string that represents your Full Name?
+
+// var myName = "Farwa Zafar";
+
+// Q3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message.
+// c) Display the message in alert box.?
+
+// var message
+// message ="Hello World"
+// alert(message)
+
+// Q4. Write a script to save student’s bio data in JS variables andshow the data in alert boxes?
+
+// var studentName ="Jhone Doe"
+// var studentAge ="15 years old"
+// var course ="Certified Mobile Application Development"
+// alert(studentName)
+// alert(studentAge)
+// alert(course)
+
+// Q5. Write a script to display the following alert using one JSvariable?
+
+// var pizza ="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+
+// Q6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation?
+
+// var email ="areh12378@gmail.com"
+// alert("My Email Address is " + email)
+
+// Q7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box?
+
+// var book ="“A smarter way to learn JavaScript”"
+// alert("I am trying to learn from the book " + book)
+
+// Q8. Write a script to display this in browser through JS?
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// Q9. Store following string in a variable and show in alert and
+// browser through JS?
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+// var symbol = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(symbol);
