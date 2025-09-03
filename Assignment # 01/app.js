6e3ff9ea7a60eef0dc4e8a45e43c8d95b9e7a567
hadiyasha10 <hadiyasheikh329@gmail.com>
2025-09-03 20:58:22 +0500

    done

---
+++ Assignment # 01/app.js
@@ -0,0 +1,15 @@
+//Q1:Write a script to greet your website visitor using JS alert box?
+alert("Hello welcome to my website");
+
+//Q2:Write a script to display following message on your web page?
+alert("Error! Please enter a valid password.");
+
+//Q3:Write a script to display following message on your web page: (Hint : Use line break)?
+alert("Welcome to JS Land...\nHappy Coding!");
+
+//Q4:Write a script to display following messages in sequence?
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+//Q5:Generate the following message through browser’s developer console?
+console.log("Hello... I can run JS through my web browser's developer console");
