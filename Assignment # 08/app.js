35d16376d909de587f35b3d9498550de1ff63f05
Hadiya Sheikh <hadiyasheikh329@gmail.com>
2025-09-28 16:15:22 +0500

    Changes done

--- Assignment # 08/app.js
+++ Assignment # 08/app.js
@@ -1,8 +1,8 @@
 // 1. Write a program that takes a character (number or string) in a variable & checks whether the given input is a
 // number, uppercase letter or lower case letter. (Hint: ASCII codes:- A=65, Z=90, a=97, z=122).
 
-// Let character = prompt("Please enter your character to checks whether the given input is a number, uppercase letter or lower case letter");
-// Let value = character.charCodeAt(0);
+// let character = prompt("Please enter your character to checks whether the given input is a number, uppercase letter or lower case letter");
+// let value = character.charCodeAt(0);
 
 // //Checking for Number
 // if (value >= 48 && value <= 57) {
@@ -25,8 +25,8 @@
 
 // 2. Write a JavaScript program that accept two integers and display the larger. Also show if the two integers are equal.
 
-// Let firstNumber=+prompt("Enter first number");
-// Let secondNumber=+prompt("Enter second number");
+// let firstNumber=+prompt("Enter first number");
+// let secondNumber=+prompt("Enter second number");
 // if(firstNumber>secondNumber){
 //     alert(`${firstNumber} is largerthan ${secondNumber}`);
 // }
@@ -39,7 +39,7 @@
 
 // 3. Write a program that takes input a number from user & state whether the number is positive, negative or zero.
 
-// Let num=+prompt("Please enter number to check whether positive, negative or zero");
+// let num=+prompt("Please enter number to check whether positive, negative or zero");
 // if(num>0){
 //     alert(`${num} is postive`)
 // }
@@ -52,7 +52,7 @@
 
 // 4. Write a program that takes a character (i.e. string of length 1) and returns true if it is a vowel, false otherwise
 
-// Let character=prompt("Enter character to check vowel or not");
+// let character=prompt("Enter character to check vowel or not");
 // if(character==='a'||character==='A'||character==='e'||character==='E'||character==='i'||character==='I'||character==='o'||character==='O'||character==='u'||character==='U'){
 //     alert(`${character} is vowel`)
 // }
@@ -68,8 +68,8 @@
 // ii. Check if both passwords are same. If they are same, show message “Correct! The password you entered matches the original password”.
 // Show “Incorrect password” otherwise.
 
-// Let password="Hadiya1234";
-// Let userPassword=prompt("Enter your password");
+// let password="Hadiya1234";
+// let userPassword=prompt("Enter your password");
 // if(userPassword===''){
 //     alert("Please enter your password");
 // }
@@ -81,16 +81,16 @@
 // }
 
 // 6. This if/else statement does not work. Try to fix it:
-// Let greeting;
-// Let hour = 13;
+// let greeting;
+// let hour = 13;
 // if (hour < 18) {
 // greeting = "Good day";
 // else
 // greeting = "Good evening";
 // }
 
-// Let greeting;
-// Let hour=13;
+// let greeting;
+// let hour=13;
 // if(hour<18){
 //     greeting="Good day";
 //     alert(greeting);
@@ -104,7 +104,7 @@
 // 7. Write a program that takes time as input from user in 24 hours clock format like: 1900 = 7pm.
 // Implement the following case using if, else & else if statements
 
-// Let time=+prompt('Please enter time in 24 hours \n clock format like: 1900 = 7pm.');
+// let time=+prompt('Please enter time in 24 hours \n clock format like: 1900 = 7pm.');
 // if(time>=000 && time<1200){
 //     alert('Good Morning')
 // }
